24d94e5b52fe34e740305d57ecbc0330173709c4
Khiem Nguyen <ngbinhkhiem1611@gmail.com>
2021-09-09 21:19:34 +0700

    update

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Function will return to client
 def home_page(request):
-    return HttpResponse('Hello World!')
+    data = {
+        "products": [{
+            "name": "Tivi 26 inch",
+            "price": "10.000",
+        }, {
+            "name": "Tivi 27 inch",
+            "price": "10.000",
+        }, {
+            "name": "Tivi 28 inch",
+            "price": "10.000",
+        }]
+    }
+
+    return render(request, 'pages/home.html', data)
